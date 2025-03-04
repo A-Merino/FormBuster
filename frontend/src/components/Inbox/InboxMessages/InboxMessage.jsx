3235248cbd828b769931219bb2ad0a222dffe0b4
RocketBlaster05 <danielx1611@gmail.com>
2025-03-04 16:55:38 -0500

    Inbox

---
+++ frontend/src/components/Inbox/InboxMessages/InboxMessage.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './InboxMessage.css'
+
+function InboxMessage() {
+
+  return (
+    <>
+      <div id="inbox-message">
+        <h2>Inbox Message</h2>
+        <text>Description of message</text>
+      </div>
+    </>
+  )
+}
+
+export default InboxMessage
