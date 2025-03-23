334dd3a64e91ad74b3b87a386c96a7cc76065b02
RocketBlaster05 <danielx1611@gmail.com>
2025-03-23 17:04:31 -0400

    Correct Hello, UserName redirection in TopBar

--- frontend/src/components/TopBar/TopBar.jsx
+++ frontend/src/components/TopBar/TopBar.jsx
@@ -8,7 +8,7 @@ function TopBar() {
     return (
         <>
             <div id="top-bar">
-                <p>Hello, <Link to="/settings">User Name</Link></p>
+                <p>Hello, <Link to="/account">User Name</Link></p>
             </div>
         </>
     )
