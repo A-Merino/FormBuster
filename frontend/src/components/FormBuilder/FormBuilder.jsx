d3142ca08bf800eee72a2f9a15e215cdbe67af46
LukaMSF <lstarcevic2022@my.fit.edu>
2025-04-06 22:50:48 -0400

    Added most basic form editor functionalities

--- frontend/src/components/FormBuilder/FormBuilder.jsx
+++ frontend/src/components/FormBuilder/FormBuilder.jsx
@@ -1,19 +1,36 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import './FormBuilder.css'
 import TopBar from "../TopBar/TopBar.jsx";
 import Menu from "../Menu/Menu.jsx";
 import Editor from "./Editor/Editor.jsx";
+import {convertToEditor, convertToDB} from "./Util/Util.jsx";
+
 function FormBuilder() {
+    const [newForm, setNewFrom] = useState({
+        name: "",
+    });
     const [forms, setForms] = useState([]);
     const [form, setForm] = useState({
         name: "",
         data: "",
     });
 
-    const handleSubmit = async e => {
+    const handleChange = (e) => {
+        setNewFrom({ ...newForm, [e.target.name]: e.target.value });
+    }
+
+    const handleNewForm = () => {
+        setForms([...forms, newForm]);
+    }
+
+    const editorRef = useRef(null);
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            form.data = document.getElementById("form").innerHTML;
+            const content = editorRef.current.getValue();
+
+            form.data = convertToDB(content);
             const response = await fetch('http://localhost:3000/api/saveForm', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
@@ -32,12 +49,15 @@ function FormBuilder() {
 
     const handleSelect = (e) => {
         const selectedFormName = e.target.value;
-        setForm({ name: selectedFormName, data: '' });  // Reset the form data when the selection changes
+        // Reset the form data when the selection changes
+        setForm({ name: selectedFormName, data: '' });
 
         // Find the form based on the name selected
         const selectedForm = forms.find((f) => f.name === selectedFormName);
         if (selectedForm) {
-            setForm({ name: selectedForm.name, data: selectedForm.data });  // Update the form with the data of the selected form
+            const convertedData = convertToEditor(selectedForm.data);
+            // Update the form with the data of the selected form
+            setForm({ name: selectedForm.name, data: convertedData });
         }
     }
 
@@ -62,7 +82,7 @@ function FormBuilder() {
         <TopBar/>
         <Menu/>
         <div id="form-builder">
-            <div>
+            <div id="form-selector">
                 <h2>Select a form</h2>
                 <select onChange={handleSelect} value={form.name}>
                     <option value="">--Select a Form--</option>
@@ -72,16 +92,29 @@ function FormBuilder() {
                         </option>
                     ))}
                 </select>
+                <label>Create new form:
+                    <input
+                        name="name"
+                        type="text"
+                        onChange={handleChange}
+                        value={newForm.name}
+                    />
+                </label>
+                <button onClick={handleNewForm} type="submit">Submit</button>
             </div>
-            <Editor>
-
-            </Editor>
-            <div id="mod-form" dangerouslySetInnerHTML={{ __html: form.data }}/>
-            {/*{form.name !== "" && (*/}
-            {/*<button onClick={handleSubmit}>*/}
-            {/*    Save Form*/}
-            {/*</button>*/}
-            {/*)}*/}
+            {form.name !== "" && (
+                <div id="form-editor">
+                    <Editor
+                        initialValue={form.data}
+                        ref={editorRef}
+                        id="form-editor"
+                        key={form.name}
+                    />
+                    <button onClick={handleSubmit}>
+                        Save Form
+                    </button>
+                </div>
+            )}
         </div>
         </>
     );
