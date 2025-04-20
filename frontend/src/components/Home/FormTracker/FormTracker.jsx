daed1606419060e8d4fac51b41a1359b6183ae4c
A-Merino <A-Merino>
2025-04-20 19:15:28 -0400

    tracker semi-fix

--- frontend/src/components/Home/FormTracker/FormTracker.jsx
+++ frontend/src/components/Home/FormTracker/FormTracker.jsx
@@ -15,31 +15,32 @@ function FormTracker() {
 
     // get all forms connected to the account
     useEffect(() => {
-        const fetchForms = async (forms) => {
-
-                try {
-                    // for each form string id 
-                    forms.map(async form => {
-                        // get the data of the form
-                        const response = await fetch(`http://localhost:3000/api/getActive`, {
-                            method :"POST",
-                            headers: {'Content-Type': 'application/json'},
-                            body: JSON.stringify({id:form})
-                        });
-                        // json the data
-                        const data = await response.json();
-                        // append to end of list
-                        setTracks([...tracks, data.form]);
-                    }
-                    );
-                } catch (error) {
-                    // if error then print
-                    console.error(error);
-                }
-        }
-        // call this for all forms
-        fetchForms(account.forms);
+        fetchForms(account.forms)
+        
     }, []);
+
+    const fetchForms = async (forms) => {
+
+            try {
+                // for each form string id 
+                forms.map(async form => {
+                    // get the data of the form
+                    const response = await fetch(`http://localhost:3000/api/getActive`, {
+                        method :"POST",
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify({id:form})
+                    });
+                    // json the data
+                    const data = await response.json();
+                    // append to end of list
+                    setTracks([...tracks, data.form]);
+                }
+                );
+            } catch (error) {
+                // if error then print
+                console.error(error);
+            }
+    }
 
 
   return (
@@ -50,7 +51,7 @@ function FormTracker() {
 
             <div id='track-holder'>
             
-            {tracks.map((form) => {
+            {tracks.length > 0 && tracks.map((form) => {
                 
                 return <TrackedItem key={form.id} data={form}/>
             })}
